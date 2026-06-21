aa44ef1482e9a1fa0565096b038abde6c120b76d
Cássio Silva <cassiossilva07@gmail.com>
2026-06-21 18:37:59 -0300

    Add caching functionality to the cache decorator
    Implement a caching decorator to optimize function calls.

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+    
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(sorted(kwargs.items())))
+        
+        if key not in cache_dict:
+            print("Calculating new result")
+            cache_dict[key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        
+        return cache_dict[key]
+    
+    return wrapper
