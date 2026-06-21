21edbdd368c32071ffcc75687c4dc4bb43d6bcc5
Cássio da Silva Pereira <cassiossilva07@gmail.com>
2026-06-21 18:46:48 -0300

    Fix flake8 lint errors: trailing whitespace and missing return type annotation

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
-from typing import Callable
+from typing import Any, Callable
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
-    
-    def wrapper(*args, **kwargs):
+
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
-        
+
         if key not in cache_dict:
             print("Calculating new result")
             cache_dict[key] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-        
+
         return cache_dict[key]
-    
+
     return wrapper
